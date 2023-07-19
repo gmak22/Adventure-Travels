bfcfc0c1ddca1a5fd97dad6fb0efe40f1a0bf9c7
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-07-19 22:03:29 +0530

    day 2 work done, responsive navbar, hero section heading and real-time scroll tracker

---
+++ Ravi_LandingPage/Ravi_JS/index.js
@@ -0,0 +1,30 @@
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+    {
+        transform: ["scaleX(0)", "scaleX(1)"],
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+);
