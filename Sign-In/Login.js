4d5360f14362010889f4db180cefdea87ee3fb8e
nirmalpatel26 <patelnirmal9977@gmail.com>
2023-07-22 01:39:28 +0530

    login page done

--- Sign-In/Login.js
+++ Sign-In/Login.js
@@ -1,9 +1,44 @@
+let username = document.querySelector("#username");
+let password = document.querySelector("#password")
+async function fetchURL() {
+    try {
+        let res = await fetch("https://adventure-travels-json-server.onrender.com/register");
+        let data = await res.json();
+        console.log(data);
+        let result=checkuser(data)
+        if (result) {
+            Swal.fire({
+                icon: 'success',
+                text: `LOGIN IS SUCCESSFUL`,
+            })
 
-async function  fetchURL(){
-    try{
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'ENTER CURRECT CREDIENCIAL',
 
+            })
+        }
     }
-    catch(error){
-        
+    catch (error) {
+        console.log(error);
     }
 }
+
+
+function checkuser(user){
+    for(let i=0; i<user.length; i++){
+        if(user[i].username===username.value&&user[i].password===password.value){
+            localStorage.setItem("username",JSON.stringify(username.value));
+            return true;
+        }
+    }
+    return false;
+}
+
+let proceed=document.querySelector(".process");
+proceed.addEventListener("click",(el)=>{
+    el.preventDefault();
+    fetchURL(); 
+})
