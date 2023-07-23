fe30a3d6f237e97616a957fbf727b29b30d72a21
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-07-24 01:23:56 +0530

    version after integrating login and signup pages with landing page

--- Sign-In/Login.js
+++ Sign-In/Login.js
@@ -5,7 +5,7 @@ async function fetchURL() {
         let res = await fetch("https://adventure-travels-json-server.onrender.com/register");
         let data = await res.json();
         console.log(data);
-        let result=checkuser(data)
+        let result = checkuser(data)
         if (result) {
             Swal.fire({
                 icon: 'success',
@@ -26,12 +26,12 @@ async function fetchURL() {
     }
 }
 
-let username1=JSON.parse(localStorage.getItem("username"))||"";
-function checkuser(user){
-    for(let i=0; i<user.length; i++){
-        if(user[i].username===username.value&&user[i].password===password.value){
-           let user=username.value
-            localStorage.setItem("username",JSON.stringify(user));
+let username1 = JSON.parse(localStorage.getItem("username")) || "";
+function checkuser(user) {
+    for (let i = 0; i < user.length; i++) {
+        if (user[i].username === username.value && user[i].password === password.value) {
+            let user = username.value
+            localStorage.setItem("username", JSON.stringify(user));
             signout(user);
             return true;
         }
@@ -39,28 +39,28 @@ function checkuser(user){
     return false;
 }
 
-let proceed=document.querySelector(".process");
-proceed.addEventListener("click",(el)=>{
+let proceed = document.querySelector(".process");
+proceed.addEventListener("click", (el) => {
     el.preventDefault();
-    fetchURL(); 
-    
+    fetchURL();
+
 })
 
-let loginuser=document.querySelector(".username")
-function signout(user){
-    if(user.length==0){
-        console.log(username1.length,"#");
-        loginuser.innerHTML="Login";
-       }else{
-           console.log(username1,"@");
-           loginuser.innerHTML=(user.toUpperCase());
-       }
+let loginuser = document.querySelector(".username")
+function signout(user) {
+    if (user.length == 0) {
+        console.log(username1.length, "#");
+        loginuser.innerHTML = "Login";
+    } else {
+        console.log(username1, "@");
+        loginuser.innerHTML = (user.toUpperCase());
+    }
 }
 signout(username1)
-let sign_out=document.querySelector(".sign-out");
-sign_out.addEventListener("click",()=>{
-    let user="";
-    localStorage.setItem("username",JSON.stringify(user));
+let sign_out = document.querySelector(".sign-out");
+sign_out.addEventListener("click", () => {
+    let user = "";
+    localStorage.setItem("username", JSON.stringify(user));
     signout(user)
 })
 
