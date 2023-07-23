3cb4b80618de5eb4d9dbd3d98731e3760e8f43c5
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-07-24 01:38:33 +0530

    version after integrating login and signup pages with landing page and fixed the scrollbar functionality

--- Sign-In/Login.js
+++ Sign-In/Login.js
@@ -1,3 +1,5 @@
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+
 let username = document.querySelector("#username");
 let password = document.querySelector("#password")
 async function fetchURL() {
@@ -64,3 +66,24 @@ sign_out.addEventListener("click", () => {
     signout(user)
 })
 
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+    {
+        transform: ["scaleX(0)", "scaleX(1)"],
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+);
