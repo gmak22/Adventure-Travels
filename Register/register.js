a83dcb68fa72fff7ebf6a96814253b8ca68e2425
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-07-24 17:48:14 +0530

    updated register.js file

--- Register/register.js
+++ Register/register.js
@@ -4,6 +4,7 @@ let firstname_list = document.querySelector(".firstname_list");
 let email_list = document.querySelector(".email_list");
 let new_password_list = document.querySelector(".new_password_list");
 let form = document.querySelector("form");
+let gender = document.querySelector(".lastname_list")
 
 
 
@@ -13,6 +14,7 @@ async function register() {
             "username": firstname_list.value,
             "password": new_password_list.value,
             "email": email_list.value,
+            "gender": gender.value
         }
         let res = await fetch("https://adventure-travels-json-server.onrender.com/register", {
             method: "POST",
@@ -93,3 +95,56 @@ function checkuser(data) {
     return true;
 
 }
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+    {
+        transform: ["scaleX(0)", "scaleX(1)"],
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+);
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
+
+let username1 = JSON.parse(localStorage.getItem("username")) || "";
+
+let loginuser = document.querySelector(".loginNav")
+let sign_out = document.querySelector(".sign-out-nav");
+
+function signout(user) {
+    if (user.length == 0) {
+        console.log(username1.length, "#");
+        loginuser.innerHTML = "Login";
+        sign_out.innerHTML = "";
+        loginuser.addEventListener("click", () => { window.location.href = "./../Sign-In/Login.html" });
+    } else {
+        console.log(username1, "@");
+        loginuser.innerHTML = (user.toUpperCase());
+    }
+}
+signout(username1)
+
+sign_out.addEventListener("click", () => {
+    let user = "";
+    localStorage.setItem("username", JSON.stringify(user));
+    signout(user)
+})
