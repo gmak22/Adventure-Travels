4cde44593b9254468ec323ff61b8f33904008456
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-07-24 10:53:17 +0530

    integration step-2

--- Register/register.js
+++ Register/register.js
@@ -1,3 +1,4 @@
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
 
 let firstname_list = document.querySelector(".firstname_list");
 let email_list = document.querySelector(".email_list");
@@ -52,7 +53,7 @@ function check(size) {
 form.addEventListener("submit", (el) => {
     el.preventDefault();
     fetchURL()
-    
+
 })
 
 async function fetchURL() {
@@ -60,8 +61,8 @@ async function fetchURL() {
         let res = await fetch("https://adventure-travels-json-server.onrender.com/register");
         let data = await res.json();
         console.log(data);
-        let result=checkuser(data);
-        if (result==false) {
+        let result = checkuser(data);
+        if (result == false) {
             Swal.fire({
                 icon: 'info',
                 title: 'Oops...',
@@ -80,15 +81,68 @@ async function fetchURL() {
     }
 }
 
-function checkuser(data){
-    for(let i=0; i<data.length; i++){
-        if(data[i].username==firstname_list.value||data[i].email==email_list.value){
-           
-             return false;
-             
-            
+function checkuser(data) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].username == firstname_list.value || data[i].email == email_list.value) {
+
+            return false;
+
+
         }
     }
     return true;
 
 }
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+    {
+        transform: ["scaleX(0)", "scaleX(1)"],
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+);
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
+
+let username1 = JSON.parse(localStorage.getItem("username")) || "";
+
+let loginuser = document.querySelector(".loginNav")
+let sign_out = document.querySelector(".sign-out-nav");
+
+function signout(user) {
+    if (user.length == 0) {
+        console.log(username1.length, "#");
+        loginuser.innerHTML = "Login";
+        sign_out.innerHTML = "";
+        loginuser.addEventListener("click", () => { window.location.href = "./../Sign-In/Login.html" });
+    } else {
+        console.log(username1, "@");
+        loginuser.innerHTML = (user.toUpperCase());
+    }
+}
+signout(username1)
+
+sign_out.addEventListener("click", () => {
+    let user = "";
+    localStorage.setItem("username", JSON.stringify(user));
+    signout(user)
+})
--- Sign-In/Login.js
+++ Sign-In/Login.js
@@ -1,3 +1,5 @@
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+
 let username = document.querySelector("#username");
 let password = document.querySelector("#password")
 async function fetchURL() {
@@ -5,7 +7,7 @@ async function fetchURL() {
         let res = await fetch("https://adventure-travels-json-server.onrender.com/register");
         let data = await res.json();
         console.log(data);
-        let result=checkuser(data)
+        let result = checkuser(data)
         if (result) {
             Swal.fire({
                 icon: 'success',
@@ -26,19 +28,73 @@ async function fetchURL() {
     }
 }
 
-
-function checkuser(user){
-    for(let i=0; i<user.length; i++){
-        if(user[i].username===username.value&&user[i].password===password.value){
-            localStorage.setItem("username",JSON.stringify(username.value));
+let username1 = JSON.parse(localStorage.getItem("username")) || "";
+function checkuser(user) {
+    for (let i = 0; i < user.length; i++) {
+        if (user[i].username === username.value && user[i].password === password.value) {
+            let user = username.value
+            localStorage.setItem("username", JSON.stringify(user));
+            signout(user);
             return true;
         }
     }
     return false;
 }
 
-let proceed=document.querySelector(".process");
-proceed.addEventListener("click",(el)=>{
+let proceed = document.querySelector(".process");
+proceed.addEventListener("click", (el) => {
     el.preventDefault();
-    fetchURL(); 
+    fetchURL();
+
 })
+
+let loginuser = document.querySelector(".username")
+let sign_out = document.querySelector(".sign-out");
+function signout(user) {
+    if (user.length == 0) {
+        console.log(username1.length, "#");
+        loginuser.innerHTML = "Login";
+        sign_out.innerHTML = "";
+        loginuser.addEventListener("click", () => { window.location.href = "./../../Sign-In/Login.html" });
+    } else {
+        console.log(username1, "@");
+        loginuser.innerHTML = (user.toUpperCase());
+        sign_out.innerHTML = "Sign-out";
+    }
+}
+signout(username1)
+
+sign_out.addEventListener("click", () => {
+    let user = "";
+    localStorage.setItem("username", JSON.stringify(user));
+    signout(user)
+})
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: "block",
+    scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+    {
+        transform: ["scaleX(0)", "scaleX(1)"],
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+);
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
